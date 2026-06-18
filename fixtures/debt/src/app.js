496ba2d37ea9981b556120c569496daf646b240c
Roger Chappel <miscanalysis@gmail.com>
2026-06-18 14:15:51 +1000

    test: cover todo triage fixtures

---
+++ fixtures/debt/src/app.js
@@ -0,0 +1,11 @@
+function login() {
+  // FIXME security release gate before public launch
+  return true;
+}
+
+function renderList() {
+  // TODO refactor duplicated list rendering
+  return [];
+}
+
+module.exports = { login, renderList };
